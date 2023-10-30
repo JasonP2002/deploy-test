207557b4f686d1545f9678d2bdbccc7969fd2971
jnp000 <jnp000@student.bham.ac.uk>
2023-10-30 15:06:38 +0000

    some changes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,12 +8,14 @@ function App() {
     <>
       <h1>Hi Charlie.</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Press me to make a number big!    {count}
-        </button>
         <p>
           <code>This is a deployment test, isn't it exciting.</code>
         </p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Press me to make a number big!    {count}
+        </button> <br /> <br />
+        <code>This really is a great website.</code> <br />
+        <code>Also when are we watching Cobra Kai. I think we said yesterday but it looks like we forgot.</code>
       </div>
     </>
   )
